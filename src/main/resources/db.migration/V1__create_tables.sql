31d3c5042ea71cce47c1b8fa44bd7a16798244bb
Алина Штейнбах <shteinbakh10@mail.ru>
2024-11-24 18:39:54 +0700

    подключена бд, созданы таблицы

---
+++ src/main/resources/db.migration/V1__create_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE temperature
+(
+    id              INTEGER,
+    device_id       VARCHAR,
+    date_time       TIMESTAMP,
+    temperature     INTEGER,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE configuration
+(
+    device_id                   VARCHAR,
+    min_critical_temperature    INTEGER,
+    max_critical_temperature    INTEGER,
+    update_interval             BIGINT,
+    PRIMARY KEY(device_id)
+);
+
+CREATE SEQUENCE sequence_temperature_id
+    INCREMENT 1
+    START 1
+    OWNED BY temperature.id;
